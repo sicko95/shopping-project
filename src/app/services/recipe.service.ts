a5b060bb161664b0e1cbe9cd7101f9da7df391a3
Veljko <sicko1995velja@gmail.com>
2020-09-09 11:04:52 +0200

    Modefining Recipe models (added Ingredients[] attribute)

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from '../models/recipe.model';
+import { Ingredient } from '../models/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,21 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simple description', 'https://www.biggerbolderbaking.com/wp-content/uploads/2019/07/15-Minute-Pizza-WS-Thumbnail.png'),
-    new Recipe('Another Test Recipe', 'This is simple description', 'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg')
+    new Recipe('Pizza', 'This is simple pizza',
+      'https://www.biggerbolderbaking.com/wp-content/uploads/2019/07/15-Minute-Pizza-WS-Thumbnail.png',
+      [
+        new Ingredient('Peperoni', 2),
+        new Ingredient('Cheese', 2),
+        new Ingredient('Dough', 1),
+
+      ]),
+    new Recipe('Corba', 'Tasty corba',
+      'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg',
+      [
+        new Ingredient('Salt', 1),
+        new Ingredient('Tommato', 3),
+        new Ingredient('Bosiljak', 4),
+      ])
   ];
 
   getRecipes() {
